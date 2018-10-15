cd91e7d9e38ee7cade2d2246040e0eb9c7244d29
gistrec <gistrec@mail.ru>
2018-10-16 02:46:18 +0700

    Фикс вывода информации в панели

--- db/users.js
+++ db/users.js
@@ -32,16 +32,20 @@ exports.findByUsername = function(username, cb) {
 exports.getBasicStatistic = function(id, cb) {
     process.nextTick(function() {
         var sql = "SELECT COUNT(*) AS data FROM `tasks` WHERE `user_id`=" + id;
-        sql += " UNION SELECT SUM(`data`) FROM `recent_activity` WHERE `type`=2 AND `user_id`=" + id;
-        sql += " UNION SELECT SUM(`like_need`) FROM `tasks` WHERE `user_id`=" + id;
+        sql += " UNION SELECT COALESCE(SUM(`data`), -1) FROM `recent_activity` WHERE `type`=2 AND `user_id`=" + id;
+        sql += " UNION SELECT COALESCE(SUM(`like_need`), -2) FROM `tasks` WHERE `user_id`=" + id;
+
         db.query(sql, function(err, rows) {
+            console.log(err);
+            console.log(rows);
             if (!err && rows.length != 0) {
                 var data = JSON.parse(JSON.stringify(rows))
 
                 var statistic = {};
                 statistic.tasks = data[0].data;
-                statistic.money = data[1].data;
-                statistic.likes = data[2].data;
+                // Говнокод - чтобы делать один запрос, а не три
+                statistic.money = data[1].data != -1 ? data[1].data : 0 ;
+                statistic.likes = data[2].data != -2 ? data[2].data : 0;
                 statistic.comments = 0;
 
                 return cb(err, statistic)
